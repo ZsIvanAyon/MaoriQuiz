2bcbcfc1eab92a954d2f6c407a1b7004417f8574
ZsIvanAyon <ac158364@avcol.school.nz>
2026-05-25 15:13:48 +1200

    I made my welcome message

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string firstName, lastName;
+
+            Console.WriteLine("What is your first name?");
+            firstName = Console.ReadLine();
+            Console.WriteLine("What is your first name?");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {firstName} {lastName}, welcome to the MaoriQuiz!");
         }
     }
 }
