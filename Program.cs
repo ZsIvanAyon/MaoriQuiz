d57ebac5693d5d0ebba8ce09ea13e461a2ee3254
ZsIvanAyon <ac158364@avcol.school.nz>
2026-05-27 12:15:31 +1200

    Added some dividers to make it look better and fixed the last name question from "what is your first name" to "what is your last name".

--- Program.cs
+++ Program.cs
@@ -6,12 +6,14 @@
         {
             string firstName, lastName;
 
+            Console.WriteLine("--------------------------------------------------------");
             Console.WriteLine("What is your first name?");
             firstName = Console.ReadLine();
-            Console.WriteLine("What is your first name?");
+            Console.WriteLine("What is your last name?");
             lastName = Console.ReadLine();
-
+            Console.WriteLine("--------------------------------------------------------");
             Console.WriteLine($"Hello {firstName} {lastName}, welcome to the MaoriQuiz!");
+            Console.WriteLine("--------------------------------------------------------");
         }
     }
 }
