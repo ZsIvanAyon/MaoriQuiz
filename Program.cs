8d7ba45b8d6871ed11d46884c80cf75c4ea2ed42
ZsIvanAyon <ac158364@avcol.school.nz>
2026-05-27 12:26:13 +1200

    Added a minimum and maximum character limit for the firstName

--- Program.cs
+++ Program.cs
@@ -9,6 +9,13 @@
             Console.WriteLine("--------------------------------------------------------");
             Console.WriteLine("What is your first name?");
             firstName = Console.ReadLine();
+            if ((firstName.Length < 2) || (firstName.Length > 16))
+            {
+                while ((firstName.Length < 2) || (firstName.Length > 16))
+                {
+                    Console.WriteLine("Invalid, your name must be between 2 and 16 characters long!");
+                }
+            }
             Console.WriteLine("What is your last name?");
             lastName = Console.ReadLine();
             Console.WriteLine("--------------------------------------------------------");
