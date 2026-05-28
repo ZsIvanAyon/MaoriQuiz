7abdab9bb73d6dbc8057054cee51111148a3526d
ZsIvanAyon <ac158364@avcol.school.nz>
2026-05-29 10:04:29 +1200

    Added a difficulty choice and a confirmation part for it

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             string firstName, lastName;
+            char difficultyChoice, choiceConfirmation;
 
             Console.WriteLine("--------------------------------------------------------");
             Console.WriteLine("What is your first name?");
@@ -35,6 +36,41 @@
             // Displays the welcome message which shows their first name and last name.
             Console.WriteLine($"Hello {firstName} {lastName}, welcome to the MaoriQuiz!");
             Console.WriteLine("--------------------------------------------------------");
+            // Displays the difficulty choosing and returns it back if they say no to the confirmation part.
+            do
+            {
+                difficultyChoice = DisplayDifficultyChoice();
+                switch (difficultyChoice)
+                {
+                    case 'E':
+                        {
+                            Console.WriteLine("Are you sure you want to pick Easy?");
+                        }
+                        break;
+                    case 'M':
+                        {
+                            Console.WriteLine("Are you sure you want to pick Medium?");
+                        }
+                        break;
+                    case 'H':
+                        {
+                            Console.WriteLine("Are you sure you want to pick Hard?");
+                        }
+                        break;
+                    default:
+
+                        break;
+                }
+                Console.WriteLine("[Y] for Yes [N] for No");
+                choiceConfirmation = Console.ReadLine().ToUpper()[0];
+            } while (choiceConfirmation == 'N');
+        static char DisplayDifficultyChoice()
+        {
+            char difficultyChoice;
+
+            Console.WriteLine("([E] for Easy, [M] for Medium, [H] for Hard) Which difficulty would you like to take the quiz in? ");
+            difficultyChoice = Console.ReadLine().ToUpper()[0];
+            return difficultyChoice;
         }
     }
 }
