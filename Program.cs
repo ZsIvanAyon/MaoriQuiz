a592c232504e3eb0de82f15e769142878db357d2
ZsIvanAyon <ac158364@avcol.school.nz>
2026-06-03 11:49:18 +1200

    Removed if loop for while loop

--- Program.cs
+++ Program.cs
@@ -11,26 +11,20 @@
             Console.WriteLine("What is your first name?");
             firstName = Console.ReadLine();
             // This checks the length and if its shorter than 3 characters or longer than 15 characters then it displays a invalid message and repeats the question.
-            if ((firstName.Length < 3) || (firstName.Length > 10) || (!firstName.All(char.IsLetter)))
+            while ((firstName.Length < 3) || (firstName.Length > 10) || (!firstName.All(char.IsLetter)))
             {
-                while ((firstName.Length < 3) || (firstName.Length > 10) || (!firstName.All(char.IsLetter)))
-                {
                     Console.WriteLine("Invalid, your name must be between 3 and 15 characters long!");
                     Console.WriteLine("What is your first name?");
                     firstName = Console.ReadLine();
-                }
             }
             Console.WriteLine("What is your last name?");
             lastName = Console.ReadLine();
             // This checks the length and if its shorter than 3 characters or longer than 15 characters then it displays a invalid message and repeats the question.
-            if ((lastName.Length < 3) || (lastName.Length > 10) || (!lastName.All(char.IsLetter)))
+            while ((lastName.Length < 3) || (lastName.Length > 10) || (!lastName.All(char.IsLetter)))
             {
-                while ((lastName.Length < 3) || (lastName.Length > 10) || (!lastName.All(char.IsLetter)))
-                {
                     Console.WriteLine("Invalid, your name must be between 3 and 15 characters long!");
                     Console.WriteLine("What is your first name?");
                     lastName = Console.ReadLine();
-                }
             }
             Console.WriteLine("--------------------------------------------------------");
             // Displays the welcome message which shows their first name and last name.
