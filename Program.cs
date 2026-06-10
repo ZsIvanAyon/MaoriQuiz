baadd7973eb2a9c3b5673c86847131eec43601ed
ZsIvanAyon <ac158364@avcol.school.nz>
2026-06-11 11:09:43 +1200

    I started on making the questions for the medium difficulty.

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@
             char difficultyChoice, choiceConfirmation;
 
             // Questions and the Answers for it
-            String[] easyQuestions = {$"Q1) What is the correct Maori greeting? \n A) Kia Ore B) Kia Ora C) Bula Vinaka D) Kamusta", "Q2) What does 'Whanau' mean in english? \n A) Family B) Food C) Respect D) Name", "Q3) What does 'Kai' mean in english? \n A) Ocean B) Person C) Food D) House", "Q4) What is does 'Aotearoa' mean in english? \n A) North Island B) New Zealand C) South Island D) Country", "Q5) What does 'Love' mean in Maori? \n A) Matariki B) Moana C) Marae D) Aroha"};
+            String[] easyQuestions = {"Q1) What is the correct Maori greeting? \n A) Kia Ore B) Kia Ora C) Bula Vinaka D) Kamusta", "Q2) What does 'Whanau' mean in english? \n A) Family B) Food C) Respect D) Name", "Q3) What does 'Kai' mean in english? \n A) Ocean B) Person C) Food D) House", "Q4) What is does 'Aotearoa' mean in english? \n A) North Island B) New Zealand C) South Island D) Country", "Q5) What does 'Love' mean in Maori? \n A) Matariki B) Moana C) Marae D) Aroha"};
             char[] easyAnswers = { 'B', 'A', 'C', 'B', 'D' };
-            String[] mediumQuestions = { };
-            char[] mediumAnswers = { };
+            String[] mediumQuestions = {"Q1) What does 'Iwi' mean in english? \n A) Tribe B) Children C) Ocean D) Forest", "Q2) What is a Waka? A) Canoe B) C) D)" };
+            char[] mediumAnswers = { 'A',  };
 
             Console.WriteLine("--------------------------------------------------------");
             firstName = DisplayAskName("first");
