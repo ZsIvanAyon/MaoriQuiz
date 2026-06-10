a88f4d966a3983b53455cbcd180559d34c9eee2b
ZsIvanAyon <ac158364@avcol.school.nz>
2026-06-11 10:50:04 +1200

    Tested to see if the method works and it does.

--- Program.cs
+++ Program.cs
@@ -15,16 +15,7 @@
             char[] mediumAnswers = { };
 
             Console.WriteLine("--------------------------------------------------------");
-            Console.WriteLine("What is your first name?");
-
-            firstName = Console.ReadLine();
-            // This checks the length and if its shorter than 3 characters or longer than 15 characters then it displays a invalid message and repeats the question.
-            while ((firstName.Length < 3) || (firstName.Length > 10) || (!firstName.All(char.IsLetter)))
-            {
-                Console.WriteLine("Invalid, your name must be between 3 and 10 characters long!");
-                Console.WriteLine("What is your first name?");
-                firstName = Console.ReadLine();
-            }
+            firstName = DisplayAskName("first");
 
             Console.WriteLine("What is your last name?");
             lastName = Console.ReadLine();
